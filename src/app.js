316684c4693b769e025b144758d1e408aa66c9cd
Piotr Tratwal <piotr.tratwal@gmail.com>
2021-11-26 14:00:40 +0100

    remove manual added dispatch

--- src/app.js
+++ src/app.js
@@ -13,18 +13,6 @@ import getVisibleExpense from './selectors/expenses'
 const store = configureStore();
 
 
-
-const expenseOne=  store.dispatch(addExpense({description:"water bill", amount:2500, createdAt:1000}))
-const expenseTwo=  store.dispatch(addExpense({description:"gas bill", amount:30, createdAt:-1000})) 
-const expenseThree=  store.dispatch(addExpense({description:"rent", amount:10, createdAt:1200})) 
-// store.dispatch(setTextFilter('bill'))
-const visibleExpenses=getVisibleExpense(store.getState().expenses, store.getState().filters)
- 
-
-// setTimeout(()=>{
-//   store.dispatch(setTextFilter('water'))
-// },5000)
-
   const jsx=(
   <Provider store={store}>
       <AppRouter/>
