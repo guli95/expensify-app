c38b30943aa6cfee91337649d909f22e02a52756
Piotr Tratwal <piotr.tratwal@gmail.com>
2021-11-28 12:14:00 +0100

    webpack config changes and seperate style.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,47 @@
 const path = require('path');
 
-module.exports = {
-  mode:"production",
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports=()=>{
+  return{
+    mode:"production",
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module:{
+      rules:[
+        {loader:"babel-loader",
+          test: /\.js$/,
+        exclude:/node_modules/},
+        {use: [MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+      ],
+          test:/\.s?css$/}
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({ 
+          filename: 'styles.css'
+      })
+  ],
+    devtool:'source-map',
+    devServer:{
+      historyApiFallback:true,
+      static:{ 
+       directory:path.join(__dirname, 'public'), 
+    }
   },
-  module:{
-    rules:[
-      {loader:"babel-loader",
-        test: /\.js$/,
-      exclude:/node_modules/},
-      {use:["style-loader", "css-loader","sass-loader"],
-        test:/\.s?css$/}
-    ],
-  },
-  devtool:'eval-cheap-module-source-map',
-  devServer:{
-    historyApiFallback:true,
-    static:{ 
-     directory:path.join(__dirname, 'public'), 
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
   }
-},
-performance: {
-  hints: false,
-  maxEntrypointSize: 512000,
-  maxAssetSize: 512000
-}
-};
+  };
+  }
+
+
+
+ 
